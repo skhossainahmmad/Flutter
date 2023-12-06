6d494cd8b4bd9ca9de10e9242d94fc6b32447803
skhossainahmmad <skhossainahmmed@gmail.com>
2023-12-06 23:47:05 +0600

    fast app

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: const Text(
+              " My App",
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 50,
+              ),
+            ),
+            centerTitle: true,
+          ),
+          body:Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.blue.shade50,
+          child: Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(21),bottomRight: Radius.circular(21),)
+
+
+              ),
+
+            ),
+          ) ,
+      ),
+      ),
+    );
+  }
+}
